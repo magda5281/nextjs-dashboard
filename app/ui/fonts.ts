b4a11168f702fb4320646289c0a7f36e209ba78f
Magdalena Gozdalik <magdagozdalik28@gmail.com>
2025-02-25 20:08:26 +0000

    fonts  update

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,7 +1,6 @@
 import { Inter, Lusitana } from 'next/font/google';
-export const inter = Inter({ subsets: ['latin'], display: 'swap' });
+export const inter = Inter({ subsets: ['latin'] });
 export const lusitana = Lusitana({
   subsets: ['latin'],
-  display: 'swap',
   weight: ['400', '700'],
 });
